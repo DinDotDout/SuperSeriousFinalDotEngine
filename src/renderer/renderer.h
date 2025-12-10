1a7ebd8c5d59e35f99e221a5b9b457bd12b5d6aa
Joan Dot Sastre <60853685+DinDotDout@users.noreply.github.com>
2025-12-10 10:06:26 +0100

    Update renderer.h fmt

--- src/renderer/renderer.h
+++ src/renderer/renderer.h
@@ -1,14 +1,14 @@
 #ifndef DOT_RENDERER_H
 #define DOT_RENDERER_H
-typedef struct RendererConfig {
+typedef struct RendererConfig{
         u64 mem_size;
         DOT_RendererBackendKind backend_kind;
-} RendererConfig;
+}RendererConfig;
 
-typedef struct DOT_Renderer {
+typedef struct DOT_Renderer{
         DOT_RendererBackendBase* backend;
         Arena arena; // NOTE: Will the renderer need this or only the backend
-} DOT_Renderer;
+}DOT_Renderer;
 
 internal void DOT_Renderer_Init(Arena* arena, DOT_Renderer* renderer, DOT_Window* window, RendererConfig config);
 internal void DOT_Renderer_Shutdown(DOT_Renderer* renderer);
