db75afb4d76fdc1a67212754efcb745808fcbd46
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2026-01-31 12:12:05 +0100

    Added arena based fmt string cstr and string8 construction

--- src/base/string.c
+++ src/base/string.c
@@ -1,21 +1,15 @@
-internal inline String8
-string8(u8 *str, u64 size){
-    String8 result = {str, size};
-    return result;
-}
-
-internal inline bool
+internal b8
 string8_equal(String8 a, String8 b){
     if(a.size == b.size){
-        bool result = MEMORY_EQUAL(a.str, b.str, a.size);
+        b8 result = MEMORY_EQUAL(a.str, b.str, a.size);
         return result;
     }
     return false;
 }
 
-internal bool
+internal b8
 string8_array_has(String8* arr, usize size, String8 b){
-    bool found = false;
+    b8 found = false;
     for(u64 i = 0; i < size; ++i){
         found = string8_equal(arr[i], b);
         if(found){
@@ -40,8 +34,55 @@ string8_array_to_str_array(Arena* arena, usize size, const String8* src){
     return dst;
 }
 
+internal String8
+string8_format_va(Arena *arena, char *fmt, va_list args){
+    va_list args2;
+    va_copy(args2, args);
+    u32 needed_bytes = dot_vsnprintf(0, 0, fmt, args) + 1;
+    String8 result = {0};
+    result.str  = PUSH_ARRAY_NO_ZERO(arena, u8, needed_bytes);
+    result.size = dot_vsnprintf((char*)result.str, needed_bytes, fmt, args2);
+    result.str[result.size] = 0;
+    va_end(args2);
+    return result;
+}
+
+internal String8
+string8_format(Arena *arena, char *fmt, ...){
+    va_list args;
+    va_start(args, fmt);
+    String8 result = string8_format_va(arena, fmt, args);
+    va_end(args);
+    return result;
+}
+
+internal char*
+cstr_format_va(Arena *arena, char *fmt, va_list args){
+    va_list args2;
+    va_copy(args2, args);
+
+    u32 needed_bytes = dot_vsnprintf(0, 0, fmt, args) + 1;
+
+    char *result = PUSH_ARRAY_NO_ZERO(arena, char, needed_bytes);
+    dot_vsnprintf(result, needed_bytes, fmt, args2);
+
+    va_end(args2);
+    return result;
+}
+
+internal char*
+cstr_format(Arena *arena, char *fmt, ...){
+    va_list args;
+    va_start(args, fmt);
+
+    char *result = cstr_format_va(arena, fmt, args);
+
+    va_end(args);
+    return result;
+}
+
 // This is good enough for now
-internal inline u64 HashFromString8(String8 string, u64 seed){
+internal u64 HashFromString8(String8 string, u64 seed){
     u64 result = seed; // raddebugger uses 5381
     for(u64 i = 0; i < string.size; ++i){
         result = ((result << 5) + result) + string.str[i];
--- src/base/string.h
+++ src/base/string.h
@@ -1,9 +1,10 @@
 #ifndef DOT_STRING_H
 #define DOT_STRING_H
+
 ////////////////////////////////////////////////////////////////
 //
 // String
-// Immutable and null terminated for char* compat/
+// Immutable and null terminated for char* compatibility
 
 typedef struct String8 {
     u8 *str;
@@ -28,9 +29,15 @@ typedef struct StringBuilder {
 #define StrArg(sv) (int)(sv).len, (sv).buff
 #define MEM_COPY_STRING8(dst, s) MEM_COPY(dst, (s).str, (s).size)
 
-internal inline String8 string8(u8 *str, u64 size);
 internal String8 str8_cstring(char *c);
-internal inline bool string8_equal(String8 a, String8 b);
-internal inline bool string8_array_has(String8* arr, usize size, String8 b);
-internal const char** string8_array_to_str_array(Arena* arena, usize size, const String8* src);
+internal String8 string8_format_va(Arena *arena, char *fmt, va_list args);
+internal String8 string8_format(Arena *arena, char *fmt, ...);
+
+internal char* cstr_format_va(Arena *arena, char *fmt, va_list args);
+internal char* cstr_format(Arena *arena, char *fmt, ...);
+
+internal b8      string8_equal(String8 a, String8 b);
+internal b8      string8_array_has(String8* arr, usize size, String8 b);
+
+internal const char**  string8_array_to_str_array(Arena* arena, usize size, const String8* src);
 #endif // DOT_STRING_H
