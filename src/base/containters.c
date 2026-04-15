3b23b0f97d7eb6ee29f8a3cb5f597547cb17139b
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2026-04-15 21:39:35 +0200

    More container fixess

--- src/base/containters.c
+++ src/base/containters.c
@@ -10,6 +10,11 @@ internal u8*
 pool_obtain(Pool *p, PoolHandle h, u32 elem_size)
 {
     u32 idx = h.handle[0];
+    if(idx == 0){
+        u8 *ptr = raw_buffer_get(p->raw_buffer, 0, elem_size);
+        MEMORY_ZERO(ptr,  elem_size);
+        return ptr;
+    }
     DOT_ASSERT(idx < p->elem_count, "Invalid pool handle");
     u32 elem_idx = p->idx_buffer[p->elem_current];
     u8 *res = raw_buffer_get(p->raw_buffer, elem_idx, elem_size);
@@ -26,21 +31,25 @@ pool_get_handle(Pool *p)
 }
 
 internal void
-pool_free_handle(Pool *p, PoolHandle h)
+pool_free_handle(Pool *p, PoolHandle h, u32 elem_size)
 {
     u32 idx = h.handle[0];
     if(idx == 0){
         return;
     }
+
+    u8 *ptr = raw_buffer_get(p->raw_buffer, p->idx_buffer[idx], elem_size);
+    MEMORY_ZERO(ptr,  elem_size);
+
     DOT_SWAP(u32, p->idx_buffer[p->elem_current], p->idx_buffer[idx]);
     --p->elem_current;
 }
 
 internal void
-pool_init(Arena *arena, Pool *p, u32 elem_count, u32 elem_size)
+pool_init(Arena *arena, Pool *p, u32 elem_count, u32 elem_size, u32 alignment)
 {
     p->elem_count = elem_count;
-    p->raw_buffer = PUSH_ARRAY_NO_ZERO(arena, u8, elem_count*elem_size);
+    p->raw_buffer = PUSH_ARRAY_ALIGNED(arena, u8, elem_count*elem_size, alignment);
     p->idx_buffer = PUSH_ARRAY_NO_ZERO(arena, u32, elem_count);
     p->elem_current = 0;
     for EACH_INDEX(i, elem_count){
