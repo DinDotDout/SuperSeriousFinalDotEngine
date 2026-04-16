8fa8167295e046d4cbf42fafa50d2554527b3c45
Joan Dot Sastre <60853685+DinDotDout@users.noreply.github.com>
2026-04-16 18:20:51 +0200

    zero on get rather than on free

--- src/base/containters.h
+++ src/base/containters.h
@@ -29,12 +29,12 @@ typedef struct Pool{
 // (jd) The ref deref crazy thing is just there to preserve the syntax of how you would call the functions
 #define POOL(T) struct{ Pool pool; T *last_accessed; }
 #define POOL_INIT(arena, tp, elem_count) pool_init((arena), &(tp)->pool, (elem_count), sizeof(*(tp)->last_accessed), ALIGNOF(*(tp)->last_accessed))
-#define POOL_GET_H(tp) pool_get_handle(&(tp)->pool)
+#define POOL_GET_H(tp) pool_get_handle(&(tp)->pool, sizeof(*(tp)->last_accessed))
 #define POOL_ACCESS_H(tp, handle) ((tp)->last_accessed = pool_obtain(&(tp)->pool, (handle), sizeof(*(tp)->last_accessed)))
-#define POOL_FREE_H(tp, handle) pool_free_handle(&(tp)->pool, (handle), sizeof(*(tp)->last_accessed))
+#define POOL_FREE_H(tp, handle) pool_free_handle(&(tp)->pool, (handle))
 
 internal void       pool_init(Arena *arena, Pool *p, u32 elem_count, u32 elem_size, u32 alignment);
 // internal void       pool_destroy(Pool *p, u32 elem_count, u32 elem_size);
-internal PoolHandle pool_get_handle(Pool *p);
-internal void       pool_free_handle(Pool *p, PoolHandle h, u32 elem_size);
+internal PoolHandle pool_get_handle(Pool *p, u32 elem_size);
+internal void       pool_free_handle(Pool *p, PoolHandle h);
 internal void      *pool_obtain(Pool *p, PoolHandle h, u32 elem_size);
