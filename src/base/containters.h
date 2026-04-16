3130d47d96900ea20627532402f157e892f70b68
Joan Dot Sastre <60853685+DinDotDout@users.noreply.github.com>
2026-04-16 19:08:02 +0200

    I should really squash all this

--- src/base/containters.h
+++ src/base/containters.h
@@ -12,26 +12,25 @@ internal u8 *raw_buffer_get(u8 raw_buffer[], i32 elem_idx, u32 elem_size);
 //     POOL(int) tp;
 //     POOL_INIT(arena, &tp, 32);
 //
-//     PoolHandle h = POOL_GET_H(&tp);
-//     int i = POOL_ACCESS_H(&tp, h);
+//     PoolHandle h = POOL_H_GET(&tp);
+//     int i = POOL_H_ACCESS(&tp, h);
 //     printf("pool int %d", i);
-//     POOL_FREE_H(&tp, h);
+//     POOL_H_FREE(&tp, h);
 // }
 
-typedef u32 PoolHandle;
 typedef struct Pool{
-    u8  *raw_buffer; // elem_size*capacity
-    PoolHandle *h_buffer; // count
-    u32 count;
-    u32 capacity;
+    u8  *raw_buffer; // elem_size * capacity
+    u32 *idx_buffer; // count; maps handle -> raw_buffer index
+    u32  count;
+    u32  capacity;
 }Pool;
 
-// (jd) The ref deref crazy thing is just there to preserve the syntax of how you would call the functions
+// (jd) The ref deref crazyness is just there to preserve the syntax of how you would call the functions
 #define POOL(T) struct{ Pool pool; T *last_accessed; }
 #define POOL_INIT(arena, tp, capacity) pool_init((arena), &(tp)->pool, (capacity), sizeof(*(tp)->last_accessed), ALIGNOF(*(tp)->last_accessed))
-#define POOL_GET_H(tp) pool_handle_get(&(tp)->pool, sizeof(*(tp)->last_accessed))
-#define POOL_ACCESS_H(tp, handle) ((tp)->last_accessed = pool_handle_access(&(tp)->pool, (handle), sizeof(*(tp)->last_accessed)))
-#define POOL_FREE_H(tp, handle) pool_handle_free(&(tp)->pool, (handle))
+#define POOL_H_GET(tp) pool_handle_get(&(tp)->pool, sizeof(*(tp)->last_accessed))
+#define POOL_H_ACCES(tp, handle) ((tp)->last_accessed = pool_handle_access(&(tp)->pool, (handle), sizeof(*(tp)->last_accessed)))
+#define POOL_H_FREE(tp, handle) pool_handle_free(&(tp)->pool, (handle))
 
 internal void       pool_init(Arena *arena, Pool *p, u32 capacity, u32 elem_size, u32 alignment);
 // internal void       pool_destroy(Pool *p, u32 elem_count, u32 elem_size);
