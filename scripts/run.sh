ce5084e43ce1cf5641e086ab236c6ced79831052
DinDotDout <60853685+DinDotDout@users.noreply.github.com>
2026-04-21 08:53:46 +0200

    renderdoc tests

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-LSAN_OPTIONS="suppressions=scripts/lsan.supp:detect_leaks=1:print_suppressions=0" \
+export LSAN_OPTIONS="suppressions=scripts/lsan.supp:detect_leaks=1:print_suppressions=0"
+# export ENABLE_VULKAN_RENDERDOC_CAPTURE=1
 ./build/dot_engine
